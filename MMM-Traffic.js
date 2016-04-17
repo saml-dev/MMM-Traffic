b69bf28ba5e33d0fba35f9eb405359d3a668bf9b
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-17 19:01:37 -0400

    reorganizing

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -1,93 +1,51 @@
 /* global Module */
 
 /* Magic Mirror
- * Module: MMM-Wunderlist
+ * Module: MMM-Traffic
  *
- * By Paul-Vincent Roll http://paulvincentroll.com
+ * By Sam Lewis https://github.com/SamLewis0602
  * MIT Licensed.
  */
 
-Module.register('MMM-Wunderlist',{
-	
+Module.register('MMM-Traffic',{
+
 	defaults: {
-		maximumEntries: 10,
-		lists: ["inbox"],
+		api_key: '',
+		mode: 'driving',
 		interval: 60,
-		fade: true,
-		fadePoint: 0.25
+		origin: '',
+		destination: ''
 	},
-	
+
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === "WUNDERLIST"){
-			if (payload.hasOwnProperty("lists")){
-			}
-		}
-		else if (notification === "WUNDERLIST_TASKS"){
-			var list_name = payload.list_name.replace(/\s+/g, '');
-			if(this.tasks.indexOf(list_name) == -1) {
-				this.tasks[list_name] = []
-				this.tasks[list_name].push(payload.tasks)
-			}
-			else {
-				this.tasks[list_name].push(payload.tasks)
-			}
+		if (notification === "TRAFFIC"){
+
 			//console.log(this.tasks.inbox[0]);
-			this.updateDom(3000);
+			this.updateDom(1000);
 		}
 	},
-	
+
 	start: function() {
 		this.tasks = []
 		this.sendSocketNotification('WUNDERLIST_CONFIG', this.config);
 		Log.info('Starting module: ' + this.name);
 	},
-	
-	getTodos: function () {
-		var tasks_to_show = []
-		
-		for (var i = 0; i < this.config.lists.length; i++) {
-			if (typeof this.tasks[this.config.lists[i].replace(/\s+/g, '')] != 'undefined'){
-				var list = this.tasks[this.config.lists[i].replace(/\s+/g, '')][0]
-				
-				for (var todo in list){
-				tasks_to_show.push(list[todo])
-				
-				}
-			} 		
-		}
-		return tasks_to_show.slice(0, this.config.maximumEntries);
-		
+
 	},
 	getDom: function() {
 		var wrapper = document.createElement("table");
 		wrapper.className = "normal small light"
-		
+
 		var todos = this.getTodos()
 		console.log(todos);
-		
+
 		for (var i = 0; i < todos.length; i++) {
 			var titleWrapper =  document.createElement("tr");
 			titleWrapper.innerHTML = todos[i];
 			titleWrapper.className = "title bright";
 			wrapper.appendChild(titleWrapper);
-			
-			// Create fade effect by MichMich (MIT)
-			if (this.config.fade && this.config.fadePoint < 1) {
-				if (this.config.fadePoint < 0) {
-					this.config.fadePoint = 0;
-				}
-				var startingPoint = todos.length * this.config.fadePoint;
-				var steps = todos.length - startingPoint;
-				if (i >= startingPoint) {
-					var currentStep = i - startingPoint;
-					titleWrapper.style.opacity = 1 - (1 / steps * currentStep);
-				}
-			}
-			// End Create fade effect by MichMich (MIT)
 		}
-		
-		
 		return wrapper;
 	}
 
