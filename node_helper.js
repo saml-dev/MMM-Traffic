1d19b971847e82b3625079a99c52f752932dc1ea
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-18 01:45:37 -0400

    added json.parse to body in request

--- node_helper.js
+++ node_helper.js
@@ -19,7 +19,7 @@ module.exports = NodeHelper.create({
     var self = this;
     request({url: this.url, method: 'GET'}, function(error, response, body) {
       if (!error && response.statusCode == 200) {
-        var commute = body.routes[0].legs[0].duration.text;
+        var commute = JSON.parse(body).routes[0].legs[0].duration.text;
         Log.error(commute);
         self.sendSocketNotification('TRAFFIC_COMMUTE', commute);
       }
