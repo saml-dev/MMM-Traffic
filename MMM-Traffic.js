c712f54770a3895d4792863d9250a400763ab96a
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-18 17:10:52 -0400

    setting up interval

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -29,13 +29,12 @@ Module.register('MMM-Traffic',{
 		this.symbols['bicycling'] = 'fa fa-bicycle';
 		this.symbols['transit'] = 'fa fa-train';
 		this.sendSocketNotification('TRAFFIC_URL', this.url);
-		var self = this;
-		setInterval(this.updateCommute, this.config.interval * 1000, self);
+		setInterval(this.updateCommute, this.config.interval * 1000);
 	},
 
-	updateCommute: function(self) {
-		Log.info(self.url);
-		self.sendSocketNotification('TRAFFIC_URL', self.url);
+	updateCommute: function() {
+		Log.info(this.url);
+		this.sendSocketNotification('TRAFFIC_URL', this.url);
 	},
 
 	getStyles: function() {
