c9f59d619a9f75eae4fbcbd247accaee9b51c9a1
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-18 01:25:59 -0400

    restructured to use node_helper for api call

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -19,32 +19,42 @@ Module.register('MMM-Traffic',{
 
 	start: function() {
 		Log.info('Starting module: ' + this.name);
-	},
-
-	getDirections: function(callback) {
-
+		this.loaded = false;
+		this.url = 'https://maps.googleapis.com/maps/api/directions/json' + this.getParams();
+		this.sendSocketNotification('TRAFFIC_URL', this.url);
 	},
 
 	getDom: function() {
-		Log.error("in getDom")
+		if (!this.loaded) {
+			var wrapper = document.createElement('div');
+			wrapper.innerHTML('Loading commute...');
+			return wrapper;
+		}
 
 		var wrapper = document.createElement("div");
 		wrapper.className = "normal medium"
 
-		var duration = this.traffic.routes[0].legs[0].duration.text;
+		var duration = this.config.commute;
 		wrapper.innerHTML = "Current commute is " + duration;
 
 		return wrapper;
-	
+
 	},
 
 	getParams: function() {
-		var params = '?';
+		var this.config.params = '?';
 		params += 'mode=' + this.config.mode;
 		params += '&origin=' + this.config.origin;
 		params += '&destination=' + this.config.destination;
 		params += '&key=' + this.config.api_key;
-		return params;
+		return this.config.params;
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'TRAFFIC_COMMUTE') {
+			this.config.commute = payload;
+			this.updateDom(1000);
+		}
 	}
 
 });
--- node_helper.js
+++ node_helper.js
@@ -12,65 +12,25 @@ var request = require('request');
 
 module.exports = NodeHelper.create({
   start: function () {
-    this.config = []
-    this.config.lists = []
-    this.config.timers = []
+    this.url = '';
   },
-  get_lists: function(callback) {
-    request({url: 'https://a.wunderlist.com/api/v1/lists', method: 'GET', headers: { 'X-Access-Token': this.config.access_token,'X-Client-ID': this.config.client_id}}, function (error, response, body) {
+
+  getCommute: function() {
+    var self = this;
+    request({url: this.url, method: 'GET'}, function(error, response, body) {
       if (!error && response.statusCode == 200) {
-        var lists = {};
-        for (var i = 0; i < JSON.parse(body).length; i++) {
-          lists[JSON.parse(body)[i].title.replace(/\s+/g, '')] = {id: JSON.parse(body)[i].id}
-        }
-        callback(lists);
+        var commute = body.routes[0].legs[0].duration.text;
+        Log.error(commute);
+        self.sendSocketNotification('TRAFFIC_COMMUTE', commute);
       }
     })
   },
 
-  get_tasks: function(id, listname, callback) {
-    request({url: 'https://a.wunderlist.com/api/v1/tasks?list_id=' + id, method: 'GET', headers: { 'X-Access-Token': this.config.access_token,'X-Client-ID': this.config.client_id}}, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var tasks = []
-        for (var i = 0; i < JSON.parse(body).length; i++) {
-          tasks.push(JSON.parse(body)[i].title)
-        }
-        callback(tasks, id, listname);
-      }
-    })
-  },
-  updateData: function (data, self, i) {
-    self.get_tasks(data[self.config.lists[i].replace(/\s+/g, '')].id, self.config.lists[i], function (data, list_id, list_name) {
-
-      self.sendSocketNotification('WUNDERLIST_TASKS', {list_id: list_id, tasks: data, list_name: list_name});
-
-     })
-  },
   // Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    const self = this;
-    if (notification === 'WUNDERLIST_CONFIG') {
-      //Remove any existing timer
-      if(this.config.timers.length > 0){
-        for (var i = 0; i < this.config.timers.length; i++) {
-          clearInterval(this.config.timers[i]);
-        }
-      }
-      this.config.lists = this.arrayUnique(this.config.lists.concat(payload.lists));
-      this.config.access_token = payload.access_token
-      this.config.client_id = payload.client_id
-      this.config.interval = payload.interval * 1000
-      this.get_lists(function (data) {
-        for (var i = 0; i < self.config.lists.length; i++) {
-
-            self.updateData(data,self, i)
-            self.config.timers.push(setInterval(self.updateData, self.config.interval, data, self, i));
-
-        }
-
-        self.sendSocketNotification('WUNDERLIST', {lists: data});
-      })
-    };
+    if (notification === 'TRAFFIC_URL') {
+      this.url = payload;
+    }
   }
 
 });
