c0ed23be70fa53e8e07e0871ca791fd7e1631cd2
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-19 14:06:40 -0400

    update readme

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -7,93 +7,92 @@
  * MIT Licensed.
  */
 
-Module.register('MMM-Traffic',{
+Module.register('MMM-Traffic', {
 
-	defaults: {
-		api_key: '',
-		mode: 'driving',
-		interval: 1000, //all modules use milliseconds
-		origin: '',
-		destination: '',
-		traffic_model: 'best_guess',
-		departure_time: 'now'
-	},
-
-	start: function() {
-    Log.info('Starting module: ' + this.name);
-    this.loaded = false;
-    this.url = 'https://maps.googleapis.com/maps/api/directions/json' + this.getParams();
-    this.symbols = {
-        'driving': 'fa fa-car',
-        'walking': 'fa fa-odnoklassniki',
-        'bicycling': 'fa fa-bicycle',
-        'transit': 'fa fa-train'
-    	};
-		this.updateCommute();
-  },
-
-    updateCommute: function() {
-    	var self = this;
-        self.sendSocketNotification('TRAFFIC_URL', self.url);
-        setTimeout(function() {
-        	self.updateCommute();
-        }, self.config.interval);
+    defaults: {
+        api_key: '',
+        mode: 'driving',
+        interval: 300000, //all modules use milliseconds
+        origin: '',
+        destination: '',
+        traffic_model: 'best_guess',
+        departure_time: 'now'
     },
 
-	getStyles: function() {
-		return ['traffic.css'];
-	},
+    start: function() {
+        Log.info('Starting module: ' + this.name);
+        this.loaded = false;
+        this.url = 'https://maps.googleapis.com/maps/api/directions/json' + this.getParams();
+        this.symbols = {
+            'driving': 'fa fa-car',
+            'walking': 'fa fa-odnoklassniki',
+            'bicycling': 'fa fa-bicycle',
+            'transit': 'fa fa-train'
+        };
+        this.updateCommute(this);
+    },
 
-	getDom: function() {
-		var wrapper = document.createElement("div");
+    updateCommute: function(self) {
+        self.sendSocketNotification('TRAFFIC_URL', self.url);
+        setTimeout(function() {
+            self.updateCommute();
+        }, self.config.interval, self);
+    },
 
-		if (!this.loaded) {
-			wrapper.innerHTML = "Loading commute...";
-			return wrapper;
-		}
+    getStyles: function() {
+        return ['traffic.css'];
+    },
 
-		var table = document.createElement("table");
-		table.className = "bright medium";
-		var row = document.createElement("tr");
+    getDom: function() {
+        var wrapper = document.createElement("div");
 
-		//symbol
-		var symbolWrapper = document.createElement("td");
-		symbolWrapper.className = 'symbol';
-		var symbol = document.createElement('span');
-		symbol.className = this.symbols[this.config.mode] + ' symbol';
-		symbolWrapper.appendChild(symbol);
-		row.appendChild(symbolWrapper);
+        if (!this.loaded) {
+            wrapper.innerHTML = "Loading commute...";
+            return wrapper;
+        }
 
-		//commute time
-		var trafficInfo = document.createElement('td');
-		trafficInfo.className = 'trafficInfo';
-		trafficInfo.innerHTML = "Current commute is " + this.config.commute;
-		row.appendChild(trafficInfo);
+        var table = document.createElement("table");
+        table.className = "bright medium";
+        var row = document.createElement("tr");
 
-		//add to wrapper
-		table.appendChild(row);
-		wrapper.appendChild(table);
-		return wrapper;
-	},
+        //symbol
+        var symbolWrapper = document.createElement("td");
+        symbolWrapper.className = 'symbol';
+        var symbol = document.createElement('span');
+        symbol.className = this.symbols[this.config.mode] + ' symbol';
+        symbolWrapper.appendChild(symbol);
+        row.appendChild(symbolWrapper);
 
-	getParams: function() {
-		var params = '?';
-		params += 'mode=' + this.config.mode;
-		params += '&origin=' + this.config.origin;
-		params += '&destination=' + this.config.destination;
-		params += '&key=' + this.config.api_key;
-		params += '&traffic_model=' + this.config.traffic_model;
-		params += '&departure_time=now';
-		return params;
-	},
+        //commute time
+        var trafficInfo = document.createElement('td');
+        trafficInfo.className = 'trafficInfo';
+        trafficInfo.innerHTML = "Current commute is " + this.config.commute;
+        row.appendChild(trafficInfo);
 
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === 'TRAFFIC_COMMUTE') {
-			Log.info('received TRAFFIC_COMMUTE');
-			this.config.commute = payload;
-			this.loaded = true;
-			this.updateDom(1000);
-		}
-	}
+        //add to wrapper
+        table.appendChild(row);
+        wrapper.appendChild(table);
+        return wrapper;
+    },
+
+    getParams: function() {
+        var params = '?';
+        params += 'mode=' + this.config.mode;
+        params += '&origin=' + this.config.origin;
+        params += '&destination=' + this.config.destination;
+        params += '&key=' + this.config.api_key;
+        params += '&traffic_model=' + this.config.traffic_model;
+        params += '&departure_time=now';
+        return params;
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'TRAFFIC_COMMUTE') {
+            Log.info('received TRAFFIC_COMMUTE');
+            this.config.commute = payload;
+            this.loaded = true;
+            this.updateDom(1000);
+        }
+    }
 
 });
