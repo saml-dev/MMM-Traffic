c66a1ac325cfd1360afb77b02d8454eca4aa8eee
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-20 17:39:14 -0400

    changed table to div

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -24,7 +24,9 @@ Module.register('MMM-Traffic', {
 
     start: function() {
         Log.info('Starting module: ' + this.name);
-        console.log(this.data.classes);
+        if (this.data.classes = 'MMM-Traffic') {
+          this.data.classes = 'bright medium';
+        }
         this.loaded = false;
         this.url = 'https://maps.googleapis.com/maps/api/directions/json' + this.getParams();
         this.symbols = {
