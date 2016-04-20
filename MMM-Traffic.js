89f53f97132bb4c40fb9e9add0f4d20835d94974
Sam Lewis <sam.lewis0602@gmail.com>
2016-04-20 15:29:44 -0400

    changed table to div

--- MMM-Traffic.js
+++ MMM-Traffic.js
@@ -24,7 +24,7 @@ Module.register('MMM-Traffic', {
 
     start: function() {
         Log.info('Starting module: ' + this.name);
-        if (!this.data.classes) {
+        if (this.data.classes === '') {
           this.data.classes = 'bright medium';
         }
         this.loaded = false;
